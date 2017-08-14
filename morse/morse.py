f74deb1cb6ebd9185531e2250830a0e8834187b7
Erik <erik.end@gmail.com>
2017-08-14 12:22:13 -0700

    done

--- morse/morse.py
+++ morse/morse.py
@@ -31,6 +31,7 @@ def decodeMorse(morseCode):
             to_return = to_return[:-1]
     except IndexError:
         pass
+    print(to_return)
     return to_return
 
 
@@ -43,15 +44,21 @@ def decodeBits(bits):
             break
     rate = 0
     rate2 = 0
+    lowest = float("inf")
     for i in range(len(bits)):
-        if bits[i] == "1" and rate2 == 0:
+        if bits[i] == "1":
+            if rate2 < lowest and rate2 > 0:
+                lowest = rate2
+            rate2 = 0
             rate += 1
             continue
         if bits[i] == "0":
+            if rate < lowest and rate > 0:
+                lowest = rate
+            rate = 0
             rate2 += 1
             continue
-        rate = rate if rate < rate2 else rate2
-        break
+    rate = lowest if lowest < float("inf") else rate
     if rate == 0:
         return '.'
     bits = bits.split("0" * rate)
@@ -81,4 +88,4 @@ def decodeBits(bits):
 if __name__ == '__main__':
     print(decodeMorse(decodeBits("00011100010101010001000000011101110101110001010111000101000111010111010001110101110000000111010101000101110100011101110111000101110111000111010000000101011101000111011101110001110101011100000001011101110111000101011100011101110001011101110100010101000000011101110111000101010111000100010111010000000111000101010100010000000101110101000101110001110111010100011101011101110000000111010100011101110111000111011101000101110101110101110")))
     print(decodeMorse(decodeBits('0100010')))
-
+    print(decodeMorse(decodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")))
