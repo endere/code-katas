c0717fe0e5663047904ead595aace90163c27840
Erik <erik.end@gmail.com>
2017-08-16 15:13:24 -0700

    working for single digits

--- twisted/twisted.py
+++ twisted/twisted.py
@@ -1,4 +1,13 @@
+
 def compute_sum(number):
-    return 1
+    pass
+
+def compute_digit(number):
+    if number == 1:
+        return number
+    else:
+        return number + compute_digit(number - 1)
 
 
+if __name__ == '__main__':
+    print(compute_sum(10))
