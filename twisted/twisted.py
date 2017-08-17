fd9cd63c4e909b35e4047d7d9ca5072f0647dd64
Erik <erik.end@gmail.com>
2017-08-17 08:31:14 -0700

    misread assignment. still working code

--- twisted/twisted.py
+++ twisted/twisted.py
@@ -1,13 +1,21 @@
 
 def compute_sum(number):
-    pass
+    print(number)
+    if len(str(number)) == 1:
+        print('here')
+        return compute_digit(number)
+    sum = 0
+    for i in range(int(str(number)[0]), 0, -1):
+        sum += 1 + compute_sum(int('9' + ('0' * (len(str(number)) - 2))))
+    sum += compute_sum(int(str(number)[1:]))
+    return sum
 
 def compute_digit(number):
-    if number == 1:
+    if number == 1 or number == 0:
         return number
     else:
         return number + compute_digit(number - 1)
 
 
 if __name__ == '__main__':
-    print(compute_sum(10))
+    print(compute_sum(11))
