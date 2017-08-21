ae194af1ba25ceec85e21aa23b3dd29baa37647d
Erik <erik.end@gmail.com>
2017-08-21 15:23:09 -0700

    functional for huge numbers... but quite slow when they get truly huge

--- large_num/large_nums.py
+++ large_num/large_nums.py
@@ -11,14 +11,28 @@ references = {
     9: [1, 9]
 }
 
+
 def last_digit(n1, n2):
     if n2 == 0:
         return 1
     num = references[n1 % 10] if type(references[n1 % 10]) == int else references[n1 % 10][n2 % 4] if len(references[n1 % 10]) == 4 else references[n1 % 10][n2 % 2]
     return num
 
+def last_multi(nums):
+    power = 0
+    for i in range(len(nums)):
+        if i == 0:
+            power = nums[-1 - i]
+        elif i == len(nums) - 1:
+            return last_digit(nums[0], power)
+        else:
+            power = nums[-1 - i] ** power
 
-    
+
+def find_mod(n1, n2):
+    return n1 ** n2
+
 if __name__ == '__main__':
-    print(last_digit(2454, 2335))
-    print(2454 ** 2335 )
+    # print(last_digit(3, 6))
+    # print(find_mod(3, 6))
+    print(last_multi([3, 4, 2]))
